4f40eba579aaada65f489adb0ae78e89f4aff38b
Chandeeshsaranb <chandeeshsaranb2020ai@gmail.com>
2026-03-11 13:09:44 +0530

    updated project on ubuntu

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,4 @@
 
-
-// import { Routes, Route } from "react-router-dom";
-// import "./App.css";
-
-// import Header from "./Navigation/Header";
-// import Footer from "./Navigation/Footer";
-// import Home from "./components/Home/Home";
-// import MarchEdit from "./marchedit/MarchEdit";
-// import ProductDetail from "./marchedit/ProductDetails";
-// import CartPage from "./Cart/cartPage";
-// import PaymentPage from "./Cart/PaymentPage";
-// import Login from "./auth/Login";
-
-// import AdminPage from "./Admin/AdminPage";
-
-// import OtpPage from "./auth/otpPage";
-// import Profile from "./auth/Profile"
-
-// function App() {
-//   return (
-//     <>
-//       <Header />
-
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/march-edit" element={<MarchEdit />} />
-//         <Route path="/product/:id" element={<ProductDetail />} />
-//         <Route path="/cart" element={<CartPage />} />
-
-//         {/* Payment */}
-//         <Route path="/payment" element={<PaymentPage />} />   {/* ✅ Added */}
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/otp" element={<OtpPage />} />
-
-//         {/* Admin Page */}
-//         <Route path="/admin" element={<AdminPage />} />
-//         <Route path="/profile" element={<Profile />} />
-//       </Routes>
-
-//       <Footer />
-//     </>
-//   );
-// }
-
-// export default App;
-
-
 import { Routes, Route, useLocation } from "react-router-dom";
 import "./App.css";
 
@@ -58,7 +11,7 @@ import CartPage from "./Cart/cartPage";
 import PaymentPage from "./Cart/PaymentPage";
 import Login from "./auth/Login";
 import AdminPage from "./Admin/AdminPage";
-import OtpPage from "./auth/otpPage";
+import OtpPage from "./auth/OtpPage";
 import Profile from "./auth/Profile";
 
 function App() {
